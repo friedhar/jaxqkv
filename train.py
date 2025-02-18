ff02a6dc85106336790f3a1b614765dc5e45ca75
friedhar <ronfrie@proton.me>
2025-02-18 20:05:50 +0200

    tinkering

--- train.py
+++ train.py
@@ -1,8 +1,23 @@
 import jax.numpy as jnp
 import jax.nn as nn
+from jax import Array
+import jax
+## q,k,v : (B, S, E)
 
-def selu(x, alpha=1.67, lmbda=1.05):
-  return lmbda * jnp.where(x > 0, x, alpha * jnp.exp(x) - alpha)
+key = jax.random.key(42)
+
+
+BATCHS_SIZE = 64
+SEQ_LEN = 8
+EMB_SIZE = 32
+
+def dot_attention(q: Array, k: Array, v: Array) -> Array:
+    lhs = nn.softmax(q.dot(k.T) / jnp.sqrt(k.shape[-1]))
+    return lhs.dot(v)
+
+
+q =  jax.random.normal(key, (BATCHS_SIZE,SEQ_LEN,EMB_SIZE))
+# k =  jax.random.normal(123, (BATCHS_SIZE,SEQ_LEN,EMB_SIZE))
 
 x = jnp.arange(5.0)
-print(selu(x))
+print(q)
