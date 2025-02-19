fb24a8fc270f666f104e95e1c11ba8c0fe25ec52
friedhar <ronfrie@proton.me>
2025-02-19 16:59:52 +0200

    fn main

--- train.py
+++ train.py
@@ -10,10 +10,23 @@ from dataclasses import dataclass
 class Config:
     vocab_size: int
     embedding_size: int 
+    seq_len: int
 
 key = jax.random.key(42)
 
 class Transformer:
     def __init__(self, config: Config) -> None:
         self.config = config
+        self.theta_emb = jax.random.normal(key, self.config.vocab_size) 
+
+    def __call__(self, x: Array) -> Array:
+        x = jnp.matmul(x, self.theta_emb)
+        return x
+
+def main():
+    print("init")
+
+if __name__ == "__main__":
+    main()
+        
 
