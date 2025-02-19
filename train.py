3ada33d5e68dfe53e6026d70c9d726b7387e8b1e
friedhar <ronfrie@proton.me>
2025-02-19 17:08:31 +0200

    another layer, incomplete

--- train.py
+++ train.py
@@ -18,6 +18,7 @@ class Transformer:
     def __init__(self, config: Config) -> None:
         self.config = config
         self.theta_emb = jax.random.normal(key, (self.config.vocab_size, self.config.embedding_size)) 
+        self.theta_qkv = jax.random.normal(key, (self.config.embedding_size, 3 * self.config.embedding_size))  ## 3 * emb_size since both q, k, v are of shape emb_size
 
     def __call__(self, x: Array) -> Array:
         assert len(x.shape) == 2
@@ -25,12 +26,21 @@ class Transformer:
 
         x = jax.nn.one_hot(x, self.config.vocab_size)
         x = jnp.matmul(x, self.theta_emb)
+
+        qkv_fused = jnp.matmul(x, self.theta_qkv)
+
+        q, k, v = jnp.split(qkv_fused, 3, axis=2)
+        print(qkv_fused)
+        print(q.shape)
+        print(k.shape)
+
+
         return x
 
 def main():
     print("init")
-    transformer = Transformer(Config(vocab_size=16, embedding_size=32, seq_len=1))
-    print(transformer(jnp.array([[1]])))
+    transformer = Transformer(Config(vocab_size=16, embedding_size=32, seq_len=4))
+    (transformer(jnp.array([[1, 2, 3, 4]])))
 
 if __name__ == "__main__":
     main()
