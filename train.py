b784015aa58661713374de6f3edb40b1414cc80b
friedhar <ronfrie@proton.me>
2025-02-19 17:28:22 +0200

    unmasked attention

--- train.py
+++ train.py
@@ -14,11 +14,24 @@ class Config:
 
 key = jax.random.key(42)
 
+class AttentionBlock:
+    def __init__(self, config: Config):
+        self.config  = config
+
+    def __call__(self, q: Array, k: Array, v: Array) -> Array:
+        lhs = jax.nn.softmax(jnp.matmul(q, jnp.swapaxes(k, -2, -1))/jnp.sqrt(k.shape[-1]), axis=1)
+
+        return jnp.matmul(lhs, v) 
+            
+        
+
 class Transformer:
     def __init__(self, config: Config) -> None:
         self.config = config
         self.theta_emb = jax.random.normal(key, (self.config.vocab_size, self.config.embedding_size)) 
         self.theta_qkv = jax.random.normal(key, (self.config.embedding_size, 3 * self.config.embedding_size))  ## 3 * emb_size since both q, k, v are of shape emb_size
+
+        self.block = AttentionBlock(config)
 
     def __call__(self, x: Array) -> Array:
         assert len(x.shape) == 2
@@ -30,9 +43,13 @@ class Transformer:
         qkv_fused = jnp.matmul(x, self.theta_qkv)
 
         q, k, v = jnp.split(qkv_fused, 3, axis=2)
-        print(qkv_fused)
+        print(x.shape)
         print(q.shape)
         print(k.shape)
+
+        x = self.block(q, k, v)
+        print(x.shape)
+        
 
 
         return x
