57ff4e7cda974452ef17677639981023272217c1
friedhar <ronfrie@proton.me>
2025-02-19 16:56:54 +0200

    def Config

--- train.py
+++ train.py
@@ -1,7 +1,19 @@
 import jax.numpy as jnp
-import jax.nn as nn
 from jax import Array
 import jax
+
+from dataclasses import dataclass
+
 ## q,k,v : (B, S, E)
 
+@dataclass
+class Config:
+    vocab_size: int
+    embedding_size: int 
+
 key = jax.random.key(42)
+
+class Transformer:
+    def __init__(self, config: Config) -> None:
+        self.config = config
+
