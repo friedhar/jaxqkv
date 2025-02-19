2af2a1e86ed8548f37d21dca72fb959c6197d352
friedhar <ronfrie@proton.me>
2025-02-19 17:02:38 +0200

    assert to make sure input is correct in runtime

--- train.py
+++ train.py
@@ -17,14 +17,20 @@ key = jax.random.key(42)
 class Transformer:
     def __init__(self, config: Config) -> None:
         self.config = config
-        self.theta_emb = jax.random.normal(key, self.config.vocab_size) 
+        self.theta_emb = jax.random.normal(key, (self.config.vocab_size, self.config.embedding_size)) 
 
     def __call__(self, x: Array) -> Array:
+        assert len(x.shape) == 2
+        assert x.shape[1] == self.config.seq_len 
+
+        x = jax.nn.one_hot(x, self.config.vocab_size)
         x = jnp.matmul(x, self.theta_emb)
         return x
 
 def main():
     print("init")
+    transformer = Transformer(Config(vocab_size=16, embedding_size=32, seq_len=1))
+    print(transformer(jnp.array([[1]])))
 
 if __name__ == "__main__":
     main()
